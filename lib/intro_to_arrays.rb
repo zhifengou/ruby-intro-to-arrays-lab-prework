b3fde712e8e2d08be7d4dfe4f5265235029f78de
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 17:51:02 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,25 @@
+def instantiate_new_array
+array=[]
+end
+def array_with_two_elements 
+  array=Array.new(2)
+end
+def first_element(array)
+  return array[0]
+end
+def third_element(array)
+  array[2]
+end
+def last_element(array)
+  array[-1]
+end
+def first_element_with_array_methods(array)
+  array.first
+end
+def last_element_with_array_methods(array)
+  array.last
+end
+def length_of_array(array)
+  array.length
+end
+  
